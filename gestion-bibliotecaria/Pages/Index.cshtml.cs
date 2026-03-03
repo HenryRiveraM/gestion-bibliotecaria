809c3f9273fdc6edf286b521e79b722cbb1d655c
Arturo10021 <arturo.guzman@ucb.edu.bo>
2026-03-03 17:38:33 -0400

    Agregar archivos de proyecto.

---
+++ gestion-bibliotecaria/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gestion_bibliotecaria.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ gestion-bibliotecaria/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gestion_bibliotecaria.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
