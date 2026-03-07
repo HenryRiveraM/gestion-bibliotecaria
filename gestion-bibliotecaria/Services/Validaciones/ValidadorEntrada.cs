ba2981db8cbafe85406986cff0013a1a5720ce6f
Juan Pablo Rivera Mercado <jpriveram01@gmail.com>
2026-03-06 22:16:42 -0400

    Correcion de archivo de validaciones

--- gestion-bibliotecaria/Services/Validaciones/ValidadorEntrada.cs
+++ gestion-bibliotecaria/Services/Validaciones/ValidadorEntrada.cs
@@ -1,6 +1,74 @@
 ﻿namespace gestion_bibliotecaria.Services.Validaciones
 {
-    public class ValidadorEntrada
+    public static class ValidadorEntrada
     {
+        public static bool EstaVacio(string valor)
+        {
+            return string.IsNullOrWhiteSpace(valor);
+        }
+
+        public static bool ExcedeLongitud(string valor, int maximo)
+        {
+            if (valor == null)
+            {
+                return false;
+            }
+
+            return valor.Length > maximo;
+        }
+
+        public static bool SoloLetras(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return false;
+            }
+
+            foreach (char c in valor)
+            {
+                if (!char.IsLetter(c) && c != ' ')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool SoloLetrasYNumeros(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return false;
+            }
+
+            foreach (char c in valor)
+            {
+                if (!char.IsLetterOrDigit(c) && c != ' ')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CodigoInventarioValido(string valor)
+        {
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                return false;
+            }
+
+            foreach (char c in valor)
+            {
+                if (!char.IsLetterOrDigit(c) && c != '-')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
