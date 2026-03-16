3ff001e08542950608defd6ff8fa86d1ea87e1d6
Juan Pablo Rivera Mercado <jpriveram01@gmail.com>
2026-03-16 13:39:39 -0400

    se agrego la logica en la interface para que las clases que hereden las sobrecarguen

--- gestion-bibliotecaria/FactoryProducts/ILibraryRepository.cs
+++ gestion-bibliotecaria/FactoryProducts/ILibraryRepository.cs
@@ -1,6 +1,12 @@
+using System.Data;
+
 namespace gestion_bibliotecaria.FactoryProducts;
 
 public interface ILibraryRepository<T>
 {
-    void DoStuff(T item);
+    void Create(T item);
+    T GetById(int id);
+    DataTable GetAll();
+    void Update(T item);
+    void Delete(int id);
 }
