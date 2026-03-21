f9e2c74788d33440497d5213e1632c36c3e0573c
ciriarte-dev <carlos.iriarte.dev@gmail.com>
2026-03-20 20:02:00 -0400

    feat(libro): add new attributes to Libro model

--- gestion-bibliotecaria/Models/Libro.cs
+++ gestion-bibliotecaria/Models/Libro.cs
@@ -5,9 +5,14 @@ public class Libro
     public int LibroId { get; set; }
     public int AutorId { get; set; }
     public string Titulo { get; set; } = string.Empty;
+    public string? ISBN  { get; set; }
     public string? Editorial { get; set; }
+    public string? Genero { get; set; }
     public string? Edicion { get; set; }
     public int? AñoPublicacion { get; set; }
+    public int? NumeroPaginas { get; set; }
+    public string? Idioma { get; set; }
+    public string? PaisPublicacion { get; set; }
     public string? Descripcion { get; set; }
     public bool Estado { get; set; }
     public DateTime FechaRegistro { get; set; }
