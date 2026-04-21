344dfc26293408ec4bc4a0881dc6c18f289ea078
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2026-04-21 03:57:36 -0400

    feat: agrega fachada de disponibilidad de ejemplar

---
+++ gestion-bibliotecaria/Aplicacion/Fachadas/IEjemplarDisponibilidadFachada.cs
@@ -0,0 +1,8 @@
+﻿using gestion_bibliotecaria.Domain.Common;
+
+namespace gestion_bibliotecaria.Aplicacion.Fachadas;
+
+public interface IEjemplarDisponibilidadFachada
+{
+    Result CambiarDisponibilidad(int ejemplarId, bool disponible, int? usuarioSesionId = null);
+}
